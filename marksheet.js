d09474d6fc6450351d6813cd608c30c7b8c5e6e7
Tehmina Saleem <162157128+Tehmina10@users.noreply.github.com>
2024-03-09 21:10:14 +0500

    My First Assignment In JavaScript.

---
+++ marksheet.js
@@ -0,0 +1,126 @@
+let rollon="JS2-048";
+let name="Tehmina Saleem";
+let teacher="Hina Imtiaz";
+let clas="Web Development";
+let english=90;
+let urdu=89;
+let math=98;
+let physics=98;
+let chemistry=90;
+let total= english+urdu+math+physics+chemistry;
+let per=total/500*100;
+let grade;
+if(per<=100 && per>=89)
+        {grade="A+";}
+else if(per<=90 && per>=80)
+         {grade="A";} 
+else if(per<=80 && per>=70)
+         {grade="B";}
+else if(per<=70 && per>=60)
+          {grade="C";}
+else if(per<=60 && per>=50)
+         {grade="D";}
+//  else {grade="U";}
+//  
+//  if  (grade="U");
+// Now creat marksheet
+document.write("Rollon:",rollon);
+document.write("<br>Name:",name);
+document.write("<br>Teacher Name:",teacher);
+document.write("<br>Class:",clas);
+document.write("<table border=4 width=100% align=center >");
+document.write("<tr><th>Subject</th> <th>Marks Obtained</th> <th>Maximum Marks</th> "); 
+document.write("<tr><td>English</td> <td>",english,"</td> <td>100</td> ");
+document.write("<tr><td>Urdu</td> <td>",urdu,"</td> <td>100</td> ");
+document.write("<tr><td>Math</td> <td>",math,"</td> <td>100</td> "); 
+document.write("<tr><td>Physics</td> <td>",physics,"</td> <td>100</td> ");
+document.write("<tr><td>Chemistry</td> <td>",chemistry,"</td> <td>100</td> ");
+document.write("<tr><td>Total</td> <td>",total,"</td> <td>500</td> ");
+document.write("<tr><td>Percentage</td> <td>",per,"</td> <td></td> ");
+document.write("<tr><td>Grade</td> <td>",grade,"</td> <td></td> " ,"<br>")
+
+
+
+// 
+
+// let roll="JS2-048";
+// let nam="Tehmina Saleem";
+// let Teacher="Hina Imtiaz";
+// let cla="Web Development";
+let computer=60;
+let islamiat=69;
+let zoology=48;
+let physic=78;
+let botany=60;
+let Total= computer+islamiat+zoology+physic+botany;
+let Per=Total/500*100;
+let Grade;
+if(Per<=100 && Per>=89)
+        {Grade="A+";}
+else if(Per<=90 && Per>=80)
+         {Grade="A";} 
+else if(Per<=80 && Per>=70)
+         {Grade="B";}
+else if(Per<=70 && Per>=60)
+          {Grade="C";}
+else if(Per<=60 && Per>=50)
+         {Grade="D";}
+//  else {grade="U";}
+//  
+//  if  (grade="U");
+// Now creat marksheet
+// document.write("Rollon:",roll);
+// document.write("<br>Name:",nam);
+// document.write("<br>Teacher Name:",Teacher);
+// document.write("<br>Class:",cla);
+document.write("<table border=4 width=100% align=center >");
+document.write("<tr><th>Subject</th> <th>Marks Obtained</th> <th>Maximum Marks</th> "); 
+document.write("<tr><td>Computer</td> <td>",computer,"</td> <td>100</td> ");
+document.write("<tr><td>Islamiat</td> <td>",islamiat,"</td> <td>100</td> ");
+document.write("<tr><td>Zoology</td> <td>",zoology,"</td> <td>100</td> "); 
+document.write("<tr><td>Physics</td> <td>",physic,"</td> <td>100</td> ");
+document.write("<tr><td>Botany</td> <td>",botany,"</td> <td>100</td> ");
+document.write("<tr><td>Total</td> <td>",Total,"</td> <td>500</td> ");
+document.write("<tr><td>Percentage</td> <td>",Per,"</td> <td></td> ");
+document.write("<tr><td>Grade</td> <td>",Grade,"</td> <td></td> ");
+
+
+
+// 
+
+
+let Computer=70;
+let Islamiat=89;
+let Zoology=66;
+let Physic=78;
+let Botany=90;
+let TOTAL= Computer+Islamiat+Zoology+Physic+Botany;
+let Perc=TOTAL/500*100;
+let GRADE;
+if(Perc<=100 && Perc>=89)
+        {GRADE="A+";}
+else if(Perc<=90 && Perc>=80)
+         {GRADE="A";} 
+else if(Perc<=80 && Perc>=70)
+         {GRADE="B";}
+else if(Perc<=70 && Perc>=60)
+          {GRADE="C";}
+else if(Perc<=60 && Perc>=50)
+         {GRADE="D";}
+//  else {grade="U";}
+//  
+// Now creat marksheet
+// document.write("Rollon:",roll);
+// document.write("<br>Name:",nam);
+// document.write("<br>Teacher Name:",Teacher);
+// document.write("<br>Class:",cla);
+document.write("<table border=4 width=100% align=center >");
+document.write("<tr><th>Subject</th> <th>Marks Obtained</th> <th>Maximum Marks</th> "); 
+document.write("<tr><td>Computer</td> <td>",Computer,"</td> <td>100</td> ");
+document.write("<tr><td>Islamiat</td> <td>",Islamiat,"</td> <td>100</td> ");
+document.write("<tr><td>Zoology</td> <td>",Zoology,"</td> <td>100</td> "); 
+document.write("<tr><td>Physics</td> <td>",Physic,"</td> <td>100</td> ");
+document.write("<tr><td>Botany</td> <td>",Botany,"</td> <td>100</td> ");
+document.write("<tr><td>Total</td> <td>",TOTAL,"</td> <td>500</td> ");
+document.write("<tr><td>Percentage</td> <td>",Perc,"</td> <td></td> ");
+document.write("<tr><td>Grade</td> <td>",GRADE,"</td> <td></td> ");
